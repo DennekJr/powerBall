d6d8e9015cc4d8f315074ee07da0454f6ede8c26
DennekJr <ohirikennedy@yahoo.com>
2020-09-02 18:44:02 +0200

    event list details

--- src/components/eventList/eventList.tsx
+++ src/components/eventList/eventList.tsx
@@ -14,7 +14,7 @@ export const EventList = () => {
           <h2>WITHOUT SIDEBAR</h2>
         </div>
       </div>
-      <div className="eventDetailBody">
+      <div className="eventDetailBody" id="eventListBody">
         <IndividualEvent
           image="https://overworld.qodeinteractive.com/wp-content/uploads/2019/10/blog-img-1.jpg"
           by="Deadre Ayton"
@@ -26,17 +26,17 @@ export const EventList = () => {
         <IndividualEvent
           image="https://media.altchar.com/prod/images/940_530/gm-13f0f1bb-b35f-4be3-858e-125a438d496a-fortnites6shadowform.jpeg"
           by="Bill Kosby"
-          date="OCTOBER 19, 2020"
+          date="MARCH 4, 2020"
           news="News"
-          title="WE'VE WON THE KINGS! WOW!"
+          title="APEX LEGENDS YEARLY TOURNAMENT IS HERE!!!"
           description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
         />
         <IndividualEvent
           image="https://ymcawichita.org/sites/default/files/styles/ffwy_prgf_banner/public/2019-08/adult-competition-concentration-929831.jpg?h=bfa41935&itok=L2jeuCkL"
-          by="Kill Billy /"
-          date="OCTOBER 19, 2020 /"
+          by="Kill Billy"
+          date="JUNE 19, 2020"
           news="News"
-          title="WE'VE WON THE KINGS! WOW!"
+          title="FIFA 20 MONTHLY FUT CHAMPIONSHIP!"
           description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
         />
       </div>
